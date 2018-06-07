cd8b0858530381c466c2a01adb22e38cd47a1ce0
Joseph Watkins <joseph.o.watkins@gmail.com>
2018-06-07 22:54:03 +0100

    wip merge blocks

--- src/simple_key_value_store/core.clj
+++ src/simple_key_value_store/core.clj
@@ -1,27 +1,47 @@
 (ns simple-key-value-store.core
-  (:refer-clojure :exclude [get set read])
-  (:require [clojure.java.io :refer [file make-parents]])
-  (:import (java.io RandomAccessFile FileOutputStream DataOutputStream File)))
+  (:refer-clojure :exclude [get set merge])
+  (:require [clojure.java.io :refer [file make-parents]]
+            [clojure.data.json :as json])
+  (:import (java.io RandomAccessFile
+                    FileOutputStream
+                    FileInputStream
+                    DataOutputStream
+                    DataInputStream
+                    File)))
+
+(def default-block-size 1024)
 
 (def data-directory "data")
 (def counter (atom 0))
 
 (def writeable-block (atom nil))
 
+(defn read-all [fname]
+  (letfn [(read-stream [s]
+            (when (> (.available s) 0)
+              (cons (json/read-str (.readUTF s)) (read-stream s))))]
+    (with-open [dis (DataInputStream. (FileInputStream. fname))]
+      (read-stream dis))))
+
 (defn read-from-file [fname offset]
   (let [raf (RandomAccessFile. fname "r")]
     (.seek raf offset)
     (let [result (.readUTF raf)]
       (.close raf)
-      result)))
+      (json/read-str result))))
 
-(defn append-to-file [fname s]
+(defn append-to-file [fname s] ;; could just be spit
   (let [bytes (.getBytes s)]
     (with-open [dos (DataOutputStream. (FileOutputStream. fname true))]
       (.writeUTF dos s))))
 
 (defn create-file! [fname]
   (.createNewFile (File. fname)))
+
+(defn gen-fname!
+  "this is wrong! good block naming scheme?"
+  []
+  (str data-directory "/f" (swap! counter inc)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; protocols
@@ -33,12 +53,15 @@
   (write-key [this key value])
   (delete-key [this key]))
 
+(defprotocol MergableBlock
+  (merge [this block]))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; types
 
 (defrecord Block [fname offsets]
   ReadableBlock
-  (read-key [_ key]
+  (read-key [this key]
     (when-let [offset (clojure.core/get offsets key)]
       (read-from-file fname offset)))
 
@@ -46,28 +69,35 @@
   (write-key [this key value]
     (let [offset (.length (File. fname))
           payload (str {key value})]
-      (println offset)
       (append-to-file fname payload)
       (->Block fname (assoc offsets key offset))))
 
   (delete-key [this key]
-    (throw (Exception. "function not implemented"))))
+    (throw (Exception. "function not implemented")))
+
+  MergableBlock
+  (merge [this block]
+    (let [kvs (reduce clojure.core/merge
+                      {}
+                      (concat (read-all this) (read-all block)))]
+      (create-file! (gen-fname!))
+      (let [fname (gen-fname!)]
+        (reduce (fn [acc [k v]]
+                  (write-key acc k v))
+                (->Block fname {})
+                kvs)))))
 
 (defn new-block
   "create a new storage block on disk, returns the in memory representation"
-  [counter-ref]
-  (let [fname (str data-directory "/f" (swap! counter inc))]
+  []
+  (let [fname (gen-fname!)]
     (create-file! fname)
     (->Block fname {})))
-
-(defn merge-blocks [block1 block2]
-  (throw (Exception. "function not implement")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; public api
 
 (defn get [key]
-  ;; TODO: tidy up?
   (read-key @writeable-block key))
 
 (defn set [key value]
